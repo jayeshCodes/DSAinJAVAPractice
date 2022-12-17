0c7066c4a029547d6334a644bc79263d4372d42a
Jayesh <1032190683@mitwpu.edu.in>
2022-12-17 22:52:16 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+//package src;
+import customds.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        arrays obj=new arrays();
+        obj.array1();
+        System.out.println("hello world");
+    }
+}
---
+++ src/customds/arrays.java
@@ -0,0 +1,9 @@
+package customds;
+
+public class arrays {
+    public void array1(){
+        int arr1[]={1,2,3,4};
+        for(int i=0;i<arr1.length;i++)
+        System.out.println(arr1[i]);
+    }
+}
